3f67e1bf65c513a7ed130a455be971fa03429236
Marco Vockner <me@marcovockner.dev>
2021-06-25 23:04:17 +0200

    feat: update dotfiles, improve lint-staged config
    lint-staged now runs prettier on all supported files instead on only predefined file types, see https://maier.tech/posts/optimizing-lint-staged-config-js-for-prettier for more information.

---
+++ lint-staged.config.js
@@ -0,0 +1,43 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable unicorn/prefer-array-flat-map */
+
+// Source: https://coding.maier.tech/posts/optimizing-lint-staged-config-js-for-prettier/
+
+const micromatch = require("micromatch")
+const prettier = require("prettier")
+
+// Figure out all extensions supported by Prettier.
+const prettierSupportedExtensions = prettier
+  .getSupportInfo()
+  .languages.map(({ extensions }) => extensions)
+  .flat()
+
+const addQuotes = (a) => `"${a}"`
+
+module.exports = (allStagedFiles) => {
+  // Match files for ESLint
+  const eslintFiles = micromatch(allStagedFiles, ["**/*.{js,ts}"], {
+    dot: true,
+  })
+
+  // Match files for Prettier
+  const prettierFiles = micromatch(
+    allStagedFiles,
+    prettierSupportedExtensions.map((extension) => `**/*${extension}`),
+    { dot: true }
+  )
+
+  // Array of linters to be run in this sequence.
+  const linters = []
+
+  // Add linters only when there are staged files for them.
+  // 'prettier --write' causes lint-staged to never terminate when prettierFiles is empty.
+
+  if (eslintFiles.length > 0) linters.push(`eslint --fix ${eslintFiles.join(" ")}`)
+
+  if (prettierFiles.length > 0)
+    linters.push(`prettier --write ${prettierFiles.map((element) => addQuotes(element)).join(" ")}`)
+
+  return linters
+}
