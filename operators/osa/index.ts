19e5b9296ee54567856156fa8d00fdf715740b01
Marco Vockner <me@marcovockner.dev>
2021-09-09 00:05:25 +0200

    feat: add osa icon & metadata

---
+++ operators/osa/index.ts
@@ -0,0 +1,23 @@
+import { IOperator } from "~/types/Operator"
+
+export const osa: IOperator = {
+  name: "Osa",
+  role: "Attacker",
+  unit: "NIGHTHAVEN",
+  ratings: {
+    armor: 2,
+    speed: 2,
+    difficulty: 2,
+  },
+  meta: {
+    gender: "o",
+    country: "hr",
+    season: "Y6S3",
+    height: 180,
+    weight: 71,
+  },
+  bio: {
+    real_name: "Anja Katarina Janković",
+    birthplace: "Split, Croatia",
+  },
+}
