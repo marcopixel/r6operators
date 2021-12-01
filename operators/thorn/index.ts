11ad21c2ae109c867203bc5782cf6bb3804cfb8f
Marco Vockner <me@marcovockner.dev>
2021-12-01 18:51:52 +0100

    feat: add thorn operator icon & metadata

---
+++ operators/thorn/index.ts
@@ -0,0 +1,23 @@
+import { IOperator } from "~/types/operator"
+
+export const thorn: IOperator = {
+  name: "Thorn",
+  role: "Defender",
+  unit: "GARDA ERU",
+  ratings: {
+    health: 2,
+    speed: 2,
+    difficulty: 1,
+  },
+  meta: {
+    gender: "f",
+    country: "ie",
+    season: "Y6S4",
+    height: 188,
+    weight: 78,
+  },
+  bio: {
+    real_name: "Brianna Skehan",
+    birthplace: "County Kildare, Ireland",
+  },
+}
