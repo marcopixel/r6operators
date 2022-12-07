b6b17413ee3a8cb4b3dc4328c2ab36062363208a
Marco Vockner <me@marcovockner.dev>
2022-12-07 19:13:52 +0100

    feat: add solis icon & metadata
    Co-authored-by: danielwerg <35052399+danielwerg@users.noreply.github.com>

---
+++ operators/solis/index.ts
@@ -0,0 +1,23 @@
+import { IOperator } from "~/types/operator"
+
+export const solis: IOperator = {
+  name: "Solis",
+  role: "Defender",
+  unit: "AFEAU",
+  ratings: {
+    health: 2,
+    speed: 2,
+    difficulty: 3,
+  },
+  meta: {
+    gender: "f",
+    country: "co",
+    season: "Y7S4",
+    height: 166,
+    weight: 65,
+  },
+  bio: {
+    real_name: "Ana Valentina Díaz",
+    birthplace: "Zipaquirå, Colombia",
+  },
+}
