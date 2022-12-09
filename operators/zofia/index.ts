4c3e5a7f25a870e9b405a7b8b988b519bd141d6a
Aidan <71147658+AidanTheJsDev@users.noreply.github.com>
2022-12-09 16:58:07 -0600

    Update index.ts

--- operators/zofia/index.ts
+++ operators/zofia/index.ts
@@ -5,8 +5,8 @@ export const zofia: IOperator = {
   role: "Attacker",
   unit: "GROM",
   ratings: {
-    health: 2,
-    speed: 2,
+    health: 3,
+    speed: 1,
     difficulty: 1,
   },
   meta: {
