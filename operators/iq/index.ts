79b3e5e85225eac7d4329ba970e31db9e0aefed2
Aidan <71147658+AidanTheJsDev@users.noreply.github.com>
2023-04-18 14:34:42 -0500

    Update index.ts

--- operators/iq/index.ts
+++ operators/iq/index.ts
@@ -3,7 +3,7 @@ import { IOperator } from "~/types/operator"
 export const iq: IOperator = {
   name: "IQ",
   role: "Attacker",
-  unit: "GSG9",
+  unit: "NIGHTHAVEN",
   ratings: {
     health: 1,
     speed: 3,
