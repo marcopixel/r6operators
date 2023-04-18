ca88c67b1b134a6f73247adaab113c98bae940e8
Aidan <71147658+AidanTheJsDev@users.noreply.github.com>
2023-04-18 14:24:32 -0500

    Updated Unit

--- operators/echo/index.ts
+++ operators/echo/index.ts
@@ -3,7 +3,7 @@ import { IOperator } from "~/types/operator"
 export const echo: IOperator = {
   name: "Echo",
   role: "Defender",
-  unit: "SAT",
+  unit: "VIPERSTRIKE",
   ratings: {
     health: 2,
     speed: 2,
