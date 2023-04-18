b13bafafe7d37903ab3002556b1f7c1da01c61a7
Aidan <71147658+AidanTheJsDev@users.noreply.github.com>
2023-04-18 14:13:40 -0500

    Update index.ts

--- operators/blitz/index.ts
+++ operators/blitz/index.ts
@@ -3,7 +3,7 @@ import { IOperator } from "~/types/operator"
 export const blitz: IOperator = {
   name: "Blitz",
   role: "Attacker",
-  unit: "GSG9",
+  unit: "VIPERSTRIKE",
   ratings: {
     health: 2,
     speed: 2,
