34b0ffcf72de9ad08a237625aa8bb938b4ed3ccd
Aidan <71147658+AidanTheJsDev@users.noreply.github.com>
2023-04-18 14:30:49 -0500

    Updated Unit

--- operators/goyo/index.ts
+++ operators/goyo/index.ts
@@ -3,7 +3,7 @@ import { IOperator } from "~/types/operator"
 export const goyo: IOperator = {
   name: "Goyo",
   role: "Defender",
-  unit: "FES",
+  unit: "REDHAMMER",
   ratings: {
     health: 2,
     speed: 2,
