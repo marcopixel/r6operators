15cf62d7cdebd51728726993adc0a91266ddc44d
Aidan <71147658+AidanTheJsDev@users.noreply.github.com>
2023-04-18 13:47:28 -0500

    Added New Operator

---
+++ operators/brava/index.ts
@@ -0,0 +1,23 @@
+import { IOperator } from "~/types/operator"
+
+export const brava: IOperator = {
+  name: "Brava",
+  role: "Attacker",
+  unit: "VIPERSTRIKE",
+  ratings: {
+    health: 1,
+    speed: 3,
+    difficulty: 3,
+  },
+  meta: {
+    gender: "f",
+    country: "br",
+    season: "Y8S1",
+    height: 167,
+    weight: 71,
+  },
+  bio: {
+    real_name: "Nayara Cardoso",
+    birthplace: "Curitiba, Brazil",
+  },
+}
