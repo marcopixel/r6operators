9855d85a8d0c04e187020fbeb99cbe255fdf5522
Aidan <71147658+AidanTheJsDev@users.noreply.github.com>
2023-04-19 13:10:50 -0500

    Updated Unit

--- operators/tachanka/index.ts
+++ operators/tachanka/index.ts
@@ -3,7 +3,7 @@ import { IOperator } from "~/types/operator"
 export const tachanka: IOperator = {
   name: "Tachanka",
   role: "Defender",
-  unit: "SPETSNAZ",
+  unit: "REDHAMMER",
   ratings: {
     health: 3,
     speed: 1,
