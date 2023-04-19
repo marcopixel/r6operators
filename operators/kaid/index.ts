e4c9f7ee0e7f40a145bc7e5ec79e8b78400e7de0
Aidan <71147658+AidanTheJsDev@users.noreply.github.com>
2023-04-19 12:52:47 -0500

    Updated Unit

--- operators/kaid/index.ts
+++ operators/kaid/index.ts
@@ -3,7 +3,7 @@ import { IOperator } from "~/types/operator"
 export const kaid: IOperator = {
   name: "Kaid",
   role: "Defender",
-  unit: "GIGR",
+  unit: "REDHAMMER",
   ratings: {
     health: 3,
     speed: 1,
