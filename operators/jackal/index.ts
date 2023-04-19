e348f414ec296b590753cf774d14340a0b949813
Aidan <71147658+AidanTheJsDev@users.noreply.github.com>
2023-04-19 08:25:22 -0500

    Update index.ts

--- operators/jackal/index.ts
+++ operators/jackal/index.ts
@@ -3,7 +3,7 @@ import { IOperator } from "~/types/operator"
 export const jackal: IOperator = {
   name: "Jackal",
   role: "Attacker",
-  unit: "GEO",
+  unit: "VIPERSTRIKE",
   ratings: {
     health: 2,
     speed: 2,
