7fd9af4e5ff86eb596d0b5fdd1fe512129f4663c
Aidan <71147658+AidanTheJsDev@users.noreply.github.com>
2023-04-19 13:08:32 -0500

    Updated Unit

--- operators/sledge/index.ts
+++ operators/sledge/index.ts
@@ -3,7 +3,7 @@ import { IOperator } from "~/types/operator"
 export const sledge: IOperator = {
   name: "Sledge",
   role: "Attacker",
-  unit: "SAS",
+  unit: "REDHAMMER",
   ratings: {
     health: 3,
     speed: 1,
