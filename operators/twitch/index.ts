9ce2fa20f3c956ba0685229b81a88d95f5cfd5e4
Aidan <71147658+AidanTheJsDev@users.noreply.github.com>
2023-04-19 13:13:27 -0500

    Updated Unit

--- operators/twitch/index.ts
+++ operators/twitch/index.ts
@@ -3,7 +3,7 @@ import { IOperator } from "~/types/operator"
 export const twitch: IOperator = {
   name: "Twitch",
   role: "Attacker",
-  unit: "GIGN",
+  unit: "WOLFGUARD",
   ratings: {
     health: 2,
     speed: 2,
