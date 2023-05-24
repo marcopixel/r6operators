557b359e7af2e9d761a67a58739e19c4ba6e41e2
Marco Vockner <me@marcovockner.dev>
2023-05-24 23:24:21 +0200

    feat: add fenrir icon & metadata
    Co-authored-by: danielwerg <35052399+danielwerg@users.noreply.github.com>

---
+++ operators/fenrir/index.ts
@@ -0,0 +1,24 @@
+import { IOperator } from "~/types/operator"
+
+export const fenrir: IOperator = {
+  name: "Fenrir",
+  role: "Defender",
+  org: "UNAFFILIATED",
+  squad: "REDHAMMER",
+  ratings: {
+    health: 2,
+    speed: 2,
+    difficulty: 3,
+  },
+  meta: {
+    gender: "m",
+    country: "se",
+    season: "Y8S2",
+    height: 176,
+    weight: 74,
+  },
+  bio: {
+    real_name: "Emil Svensson",
+    birthplace: "Uppsala, Sweden",
+  },
+}
