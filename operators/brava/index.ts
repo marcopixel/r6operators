afc28fa35fbb157e7e0d1d2ff1e04778dc45180f
Marco Vockner <me@marcovockner.dev>
2023-05-24 23:22:07 +0200

    feat: add brava icon & metadata
    Co-authored-by: danielwerg <35052399+danielwerg@users.noreply.github.com>

---
+++ operators/brava/index.ts
@@ -0,0 +1,24 @@
+import { IOperator } from "~/types/operator"
+
+export const brava: IOperator = {
+  name: "Brava",
+  role: "Attacker",
+  org: "COT",
+  squad: "VIPERSTRIKE",
+  ratings: {
+    health: 1,
+    speed: 3,
+    difficulty: 3,
+  },
+  meta: {
+    gender: "f",
+    country: "br",
+    season: "Y8S1",
+    height: 170,
+    weight: 71,
+  },
+  bio: {
+    real_name: "Nayara Cardoso",
+    birthplace: "Curitiba, Brazil",
+  },
+}
