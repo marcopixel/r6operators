5f5f2b9a2bd59497383535c9b9155c0d26707aa1
Marco Vockner <me@marcovockner.dev>
2023-09-04 22:07:27 +0200

    feat: add ram icon & metadata

---
+++ operators/ram/index.ts
@@ -0,0 +1,24 @@
+import { IOperator } from "~/types/operator"
+
+export const ram: IOperator = {
+  name: "Ram",
+  role: "Attacker",
+  org: "35th CB",
+  squad: "REDHAMMER",
+  ratings: {
+    health: 3,
+    speed: 1,
+    difficulty: 2,
+  },
+  meta: {
+    gender: "f",
+    country: "kr",
+    season: "Y8S3",
+    height: 178,
+    weight: 68,
+  },
+  bio: {
+    real_name: "Bo-Ram Choi",
+    birthplace: "Busan, South Korea",
+  },
+}
