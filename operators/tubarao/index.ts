d924845f279b2910300468b3291fe4a89fcccbe4
Marco Vockner <me@marcovockner.dev>
2023-11-22 21:04:45 +0100

    feat: add tubarão icon & metadata
    Co-authored-by: danielwerg <35052399+danielwerg@users.noreply.github.com>

---
+++ operators/tubarao/index.ts
@@ -0,0 +1,24 @@
+import { IOperator } from "~/types/operator"
+
+export const tubarao: IOperator = {
+  name: "Tubarão",
+  role: "Defender",
+  org: "DAE",
+  squad: "WOLFGUARD",
+  ratings: {
+    health: 2,
+    speed: 2,
+    difficulty: 2,
+  },
+  meta: {
+    gender: "u",
+    country: "pt",
+    season: "Y8S4",
+    height: 173,
+    weight: 69,
+  },
+  bio: {
+    real_name: "Isaac Nunes Oliveira",
+    birthplace: "Ponta Delgada, Portugal",
+  },
+}
