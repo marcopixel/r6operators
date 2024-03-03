0af085d0130f794ed69ef3c03d63773eda77aca1
Marco Vockner <me@marcovockner.dev>
2024-03-03 10:36:46 +0100

    feat: add deimos icon & metadata

---
+++ operators/deimos/index.ts
@@ -0,0 +1,24 @@
+import { IOperator } from "~/types/operator"
+
+export const deimos: IOperator = {
+  name: "Deimos",
+  role: "Attacker",
+  org: "KERES LEGION",
+  squad: "UNAFFILIATED",
+  ratings: {
+    health: 2,
+    speed: 2,
+    difficulty: 2,
+  },
+  meta: {
+    gender: "m",
+    country: "us",
+    season: "Y9S1",
+    height: 186,
+    weight: 94,
+  },
+  bio: {
+    real_name: "Gerald Morris",
+    birthplace: "Birmingham, Alabama, USA",
+  },
+}
