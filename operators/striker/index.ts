f6d4750cbf4cb5ba15d58c289bfaece070fb1bc4
danielwerg <35052399+danielwerg@users.noreply.github.com>
2024-05-31 21:56:45 +0300

    feat: add sentry and striker metadata and icon (#58)
    * feat: add sentry and striker metadata
    
    * feat: add sentry icon
    
    * fix: sentry icon colors
    
    * feat: add striker icon

---
+++ operators/striker/index.ts
@@ -0,0 +1,24 @@
+import { IOperator } from "~/types/operator"
+
+export const striker: IOperator = {
+  name: "Striker",
+  role: "Attacker",
+  org: "ROS",
+  squad: "none",
+  ratings: {
+    health: 2,
+    speed: 2,
+    difficulty: 1,
+  },
+  meta: {
+    gender: "u",
+    country: "none",
+    season: "Y9S2",
+    height: 0,
+    weight: 0,
+  },
+  bio: {
+    real_name: "Undefined",
+    birthplace: "Undefined",
+  },
+}
