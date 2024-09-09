30d66d3e777a8125b7c6036bd638d733aa6076b1
Jannik Junker <jannik.junker2018@t-online.de>
2024-09-09 22:29:27 +0200

    feat: add skopos metadata and icon

---
+++ operators/skopos/index.ts
@@ -0,0 +1,23 @@
+import { IOperator } from "~/types/operator"
+
+export const ace: IOperator = {
+  name: "Skopós",
+  role: "Defender",
+  unit: "WOLFGUARD",
+  ratings: {
+    health: 2,
+    speed: 2,
+    difficulty: 2,
+  },
+  meta: {
+    gender: "f",
+    country: "Greek",
+    season: "Y9S3",
+    height: 167,
+    weight: 58,
+  },
+  bio: {
+    real_name: "Kure Galanos",
+    birthplace: "Nicosia, Cyprus",
+  },
+}
