aa570e71e79539e5870d6e4196ed84cfb0af97fa
Jannik Junker <geisterhauserde@gmail.com>
2024-09-21 13:50:18 +0200

    feat: add skopos metadata and icon (#59)
    * feat: add skopos metadata and icon
    
    * fix: switch to country code
    
    Co-authored-by: Suspense <64612795+AlmostSuspense@users.noreply.github.com>
    
    * fix: adjust weight of skopos
    
    Co-authored-by: Suspense <64612795+AlmostSuspense@users.noreply.github.com>
    
    * fix: org, unit and export name adjustments
    
    ---------
    
    Co-authored-by: Suspense <64612795+AlmostSuspense@users.noreply.github.com>

---
+++ operators/skopos/index.ts
@@ -0,0 +1,24 @@
+import { IOperator } from "~/types/operator"
+
+export const skopos: IOperator = {
+  name: "Skopós",
+  role: "Defender",
+  org: "EKAM",
+  squad: "WOLFGUARD",
+  ratings: {
+    health: 2,
+    speed: 2,
+    difficulty: 2,
+  },
+  meta: {
+    gender: "f",
+    country: "gr",
+    season: "Y9S3",
+    height: 167,
+    weight: 55,
+  },
+  bio: {
+    real_name: "Kure Galanos",
+    birthplace: "Nicosia, Cyprus",
+  },
+}
