f48b77fa40ccef381ba62f4fc2a5e2c138701212
Denny <112124445+ynn-dev@users.noreply.github.com>
2025-08-19 11:46:45 +0200

    feat: add denari metadata

---
+++ operators/denari/index.ts
@@ -0,0 +1,24 @@
+import { IOperator } from "~/types/operator"
+
+export const denari: IOperator = {
+  name: "Denari",
+  role: "Defender",
+  org: "NIGHTHAVEN",
+  squad: "NIGHTHAVEN",
+  ratings: {
+    health: 1,
+    speed: 3,
+    difficulty: 3,
+  },
+  meta: {
+    gender: "m",
+    country: "ch",
+    season: "Y10S3",
+    height: 190,
+    weight: 73,
+  },
+  bio: {
+    real_name: "Leon Winzenried",
+    birthplace: "Lugano, Switzerland",
+  },
+}
